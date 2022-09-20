157b9e8e4a27c8e9a9f30ab9e9a13882d023a2f3
tartas1995 <tartas1995@gmail.com>
2022-09-20 21:24:51 +0200

    color over get

--- index.js
+++ index.js
@@ -17,8 +17,23 @@ const gameConfig = {
     pixelsPerBlock: 20,
 }
 
+function findGetParameter(parameterName) {
+    let result = null,
+        tmp = [];
+    let items = location.search.substring(1).split("&");
+    for (let index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
+
 if (!gameConfig.displayScore) {
     document.querySelector('#score').style.display = 'none';
+} else {
+    const color = findGetParameter('color');
+    document.querySelector('#score').style.color = !!color ? `#${color}` : 'white';
 }
 
 const messageChannel = new MessageChannel();
